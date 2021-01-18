8d6336026cb74394bbe9e8e0a298adce75435c39
Bryant Kelley <bryant@bryantkelley.com>
2021-01-18 14:02:28 -0600

    Remove gradient background on banner, change card to separate cards

--- src/components/home.js
+++ src/components/home.js
@@ -4,10 +4,16 @@ export default function Home() {
   const modifers = ['terrible', 'horrible', 'no good', 'very bad'];
   const randomNumber = Math.floor(Math.random() * modifers.length);
   return (
-    <div className="mt-5 neumorph-card">
-      <h1>Hi, I'm Bryant.</h1>
-      <h2>I'm a software engineer.</h2>
-      <h3>This is my <code>{modifers[randomNumber]}</code> website.</h3>
-    </div>
+    <>
+      <div className="mt-5 neumorph-card">
+        <h1>Hi, I'm Bryant.</h1>
+      </div>
+      <div className="mt-5 neumorph-card">
+        <h2>I'm a software engineer.</h2>
+      </div>
+      <div className="mt-5 neumorph-card">
+        <h3>This is my <code>{modifers[randomNumber]}</code> website.</h3>
+      </div>
+    </>
   );
 }
