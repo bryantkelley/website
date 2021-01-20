a812c2051f706ecdae74303fa71be60e159f4db4
Bryant Kelley <bryant@bryantkelley.com>
2021-01-19 18:52:22 -0600

    Add rows and cols

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 export default function Home() {
   const modifers = ['terrible', 'horrible', 'no good', 'very bad'];
   const randomNumber = Math.floor(Math.random() * modifers.length);
   return (
-    <>
-      <div className="mt-5 neumorph-card">
-        <h1>Hi, I'm Bryant.</h1>
-      </div>
-      <div className="mt-5 neumorph-card">
-        <h2>I'm a software engineer.</h2>
-      </div>
-      <div className="mt-5 neumorph-card">
-        <h3>This is my <code>{modifers[randomNumber]}</code> website.</h3>
-      </div>
-    </>
+    <Row>
+      <Col>
+        <div className="mt-5 neumorph-card">
+          <h1>Hi, I'm Bryant.</h1>
+        </div>
+        <div className="mt-5 neumorph-card">
+          <h2>I'm a software engineer.</h2>
+        </div>
+      </Col>
+      <Col>
+        <div className="mt-5 neumorph-card">
+          <h2>This is my</h2>
+          <h2>
+            <code>{modifers[randomNumber]}</code>
+          </h2>
+          <h2>website.</h2>
+        </div>
+      </Col>
+    </Row>
   );
 }
