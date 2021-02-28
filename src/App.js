0f69ec9585bac3e47f601970ed8076a119106e40
Bryant Kelley <bryant@bryantkelley.com>
2021-02-28 14:38:39 -0600

    Change dark mode colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container } from 'react-bootstrap';
 import Home from './components/home';
 import Header from './components/header';
-import { Container } from 'react-bootstrap';
 import Appearances from './components/appearances';
 
 function App() {
