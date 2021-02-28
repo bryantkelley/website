86d0201a8eb8b86fcaa785a56e80d971ce62e0f0
Bryant Kelley <bryant@bryantkelley.com>
2021-02-28 14:14:07 -0600

    Add dark mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Home from './components/home';
 import Header from './components/header';
 import { Container } from 'react-bootstrap';
