05917a531bbf0d8ada8c7c408cf3d8fc49cd7816
Bryant Kelley <bryant@bryantkelley.com>
2021-06-19 16:13:19 -0500

    Update layout

--- src/components/home.js
+++ src/components/home.js
@@ -6,26 +6,18 @@ export default function Home() {
   const modifers = ['terrible', 'horrible', 'no good', 'very bad'];
   const randomNumber = Math.floor(Math.random() * modifers.length);
   return (
-    <Row>
-      <Col xs={12} sm={8} md={6} lg={4}>
+    <Row className="justify-content-center align-items-center">
+      <Col xs={12} md={6} lg={4}>
         <Portrait />
       </Col>
       <Col>
-        <div className="mt-5">
           <h1>Hi, I'm Bryant.</h1>
-        </div>
-        <div className="mt-5">
           <h2>I'm a software engineer.</h2>
-        </div>
-      </Col>
-      <Col>
-        <div className="mt-5">
-          <h2>This is my</h2>
-          <h2>
+          <h3>
+            {'This is my '}
             <code>{modifers[randomNumber]}</code>
-          </h2>
-          <h2>website.</h2>
-        </div>
+            {' website.'}
+          </h3>
       </Col>
     </Row>
   );
