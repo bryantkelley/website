a04c8640cf655fb98a912ca29a04cf433eae1bd8
Bryant Kelley <bryant@bryantkelley.com>
2021-06-21 19:54:46 -0500

    Change header tags

--- src/components/appearances.js
+++ src/components/appearances.js
@@ -6,25 +6,25 @@ export default function Appearances() {
     <Row className="mt-5">
       <Col xs={12}>
         <h3>Guest Appearances</h3>
-        <h5>Oops, my friends accidentally let me on their shows.</h5>
+        <h4>Oops, my friends accidentally let me on their shows.</h4>
         <Row>
           <Col xs={12} md={6}>
-            <h4>
+            <h5>
               <code>I Have No Idea What I'm Doing</code>
-            </h4>
+            </h5>
             <a href="https://goodstuff.network/noidea/64">
-              <h5>Pop Off Episode 3 - "Milk Tubes"</h5>
+              Pop Off Episode 3 - "Milk Tubes"
             </a>
           </Col>
           <Col xs={12} md={6}>
-            <h4>
+            <h5>
               <code>Idle Curiosities</code>
-            </h4>
+            </h5>
             <a href="https://idlecuriosities.com/episodes/033-the-first-of-many-bad-things-to-come-ft-bryant-kelley">
-              <h5>033 - the first of many bad things to come ft. bryant kelley</h5>
+              033 - the first of many bad things to come ft. bryant kelley
             </a>
             <a href="https://idlecuriosities.com/episodes/037-the-cat-knows-how-fat-it-is-live">
-              <h5>037 - the cat knows how fat it is (live!)</h5>
+              037 - the cat knows how fat it is (live!)
             </a>
           </Col>
         </Row>
